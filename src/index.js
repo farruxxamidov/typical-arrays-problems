d9fafec80ff2d8c86f4bb8d5cf7a399e4fdbf69c
farruxxamidov <farruxxamidov@gmail.com>
2020-03-19 09:29:42 +0500

    feat: resolve task 'typical-arrays-problems'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    let min = Number.POSITIVE_INFINITY;
+    for (let i=0; i<array.length; i++) {
+        if (min > array[i]) {
+            min = array[i];
+        }
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    let max = Number.NEGATIVE_INFINITY;
+    for (let i=0; i<array.length; i++) {
+        if (max < array[i]) {
+            max = array[i];
+        }
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    let avg=0;
+    for (let i=0; i<array.length; i++) {
+        avg += array[i];
+    }
+    avg = avg/array.length;
+    return avg; 
 }
